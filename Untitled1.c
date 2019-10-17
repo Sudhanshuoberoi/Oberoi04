d28c7f66204e9fa141f50c2864f724352cd16e1d
Sudhanshuoberoi <56446678+Sudhanshuoberoi@users.noreply.github.com>
2019-10-17 22:50:48 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<time.h>
+int main()
+{
+  time_t now;
+  struct tm *tm_now;
+  int hours,min,sec;
+  now=time(NULL);   //initialize time with zero
+  tm_now=localtime(&now);
+  hours=tm_now->tm_hour;
+  min=tm_now->tm_min;
+  sec=tm_now->tm_sec;
+  printf("time is:- %d:%d:%d",hours,min,sec);
+
+  }
+

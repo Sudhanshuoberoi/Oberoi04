92b686435cd3165ed0efa6799a461c8330e35df4
rahul-1303 <56694318+rahul-1303@users.noreply.github.com>
2019-10-17 23:22:42 +0530

    new changes

--- Untitled1.c
+++ Untitled1.c
@@ -3,18 +3,25 @@
 #include<conio.h>
 int main()
 {
-  time_t now;
-  struct tm *tm_now;
-  int hours,min,sec;
-  now=time(NULL);   //initialize time with zero
-  tm_now=localtime(&now);
-  hours=tm_now->tm_hour;
-  min=tm_now->tm_min;
-  sec=tm_now->tm_sec;
-  printf("time is:- %d:%d:%d",hours,min,sec);
-  printf("Working with project");
-  printf("Computer Science Engineer");
-  printf("Hello World");
+  int a;
+  printf("Enter the value of a: ");
+  scanf("%d",&a);
+  if(a<10)
+  {
+    time_t now;
+    struct tm *tm_now;
+    int hours,min,sec;
+    now=time(NULL);   //initialize time with zero
+    tm_now=localtime(&now);
+    hours=tm_now->tm_hour;
+    min=tm_now->tm_min;
+    sec=tm_now->tm_sec;
+    printf("time is:- %d:%d:%d",hours,min,sec);
+    printf("Working with project");
+    printf("Computer Science Engineer");
+  }
+  else
+    printf("Hello World");
   }
 
 
